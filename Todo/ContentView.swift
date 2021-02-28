45a363c788440d2402d1797eb8a095a78c69c48e
lotarev <foermanDT@gmail.com>
2021-02-28 16:05:25 +0300

    add swift file

---
+++ Todo/ContentView.swift
@@ -0,0 +1,80 @@
+//
+//  ContentView.swift
+//  Todo
+//
+//  Created by Виктор Лотарев on 27.02.2021.
+//
+
+import SwiftUI
+import Combine
+
+
+struct ContentView: View {
+    
+    @ObservedObject var taskStore = TaskStore()
+    @State var todoText : String = ""
+    
+    func addTodo(todo: String) -> Void {
+        if self.todoText != "" {
+            taskStore.task.append(
+                Task(
+                    id: taskStore.task.count + 1,
+                    text: todoText
+                ))
+            self.todoText = ""
+        }else{
+      
+        }
+    }
+    
+    var searchBar : some View{
+        HStack {
+        TextField(
+            "Текст заметки...",
+            text:self.$todoText
+        )
+                Button( action: {
+                    self.addTodo(todo: self.todoText)
+                }, label: {
+                    Text("Добавить")
+                        .foregroundColor(.red)
+                        
+            })
+        }
+    }
+    
+    var body: some View {
+        NavigationView{
+            VStack{
+                searchBar.padding()
+                List{
+                    ForEach(self.taskStore.task){ task in
+                    Text(task.text)
+                    }
+                    .onMove(perform: self.move)
+                    .onDelete(perform: self.delete)
+                }
+            }
+            .navigationBarTitle("Заметки")
+            .navigationBarItems(trailing: EditButton())
+        }
+    }
+    
+    
+    func move(from fromIndex: IndexSet, to toIndex: Int) ->Void{
+        taskStore.task.move(
+            fromOffsets: fromIndex,
+            toOffset:toIndex
+        )
+    }
+    
+    func delete(at index: IndexSet) -> Void {
+        taskStore.task.remove(atOffsets: index)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Виктор Лотарев on 27.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
