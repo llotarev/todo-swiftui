6781213bcd94cebb890659ae8ac89ad8a87788ff
lotarev <foermanDT@gmail.com>
2021-03-01 23:26:34 +0300

    change layout components 

--- Todo/ContentView.swift
+++ Todo/ContentView.swift
@@ -14,52 +14,64 @@ struct ContentView: View {
     @ObservedObject var taskStore = TaskStore()
     @State var todoText : String = ""
     
-    func addTodo(todo: String) -> Void {
-        if self.todoText != "" {
-            taskStore.task.append(
-                Task(
-                    id: taskStore.task.count + 1,
-                    text: todoText
-                ))
-            self.todoText = ""
-        }else{
-      
-        }
-    }
-    
     var searchBar : some View{
+        
         HStack {
-        TextField(
-            "Текст заметки...",
-            text:self.$todoText
-        )
-                Button( action: {
-                    self.addTodo(todo: self.todoText)
-                }, label: {
-                    Text("Добавить")
+            TextField("Текст заметки...", text: self.$todoText)
+                .frame( height: 48, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .padding(.horizontal, 12)
+                .background(Color.white)
+                .cornerRadius(48)
+            
+            Button(action: {
+                self.add()
+            }, label: {
+                Text("Add")
+                    .frame( height: 48, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .padding(.horizontal, 24)
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(48)
             })
         }
     }
     
     var body: some View {
-        NavigationView{
-            VStack{
-                searchBar.padding()
-                List{
-                    ForEach(self.taskStore.task){ task in
-                    Text(task.text)
+        ZStack{
+            
+            Color.black.ignoresSafeArea(.all)
+            
+            NavigationView{
+                VStack{
+                    List{
+                        ForEach(self.taskStore.task){ task in
+                            Text(task.text)
+                        }
+                        .onMove(perform: self.move)
+                        .onDelete(perform: self.delete)
                     }
-                    .onMove(perform: self.move)
-                    .onDelete(perform: self.delete)
+                    searchBar.padding()
                 }
+                .navigationBarTitle("Заметки")
+                .navigationBarItems(trailing: EditButton())
             }
-            .navigationBarTitle("Заметки")
-            .navigationBarItems(trailing: EditButton())
+            
         }
     }
     
     
-    func move(from fromIndex: IndexSet, to toIndex: Int) ->Void{
+    func add() {
+        if self.todoText != "" {
+            taskStore.task.append(
+                Task(
+                    id:taskStore.task.count + 1,
+                    text:self.todoText
+                ))
+            self.todoText = ""
+        }else{}
+    }
+    
+    func move(from fromIndex: IndexSet, to toIndex: Int){
         taskStore.task.move(
             fromOffsets: fromIndex,
             toOffset:toIndex
